23708259464fbeda17591b43d5ccac6975255381
Kihara Ryou <ryoukayuu@gmail.com>
2019-12-27 10:33:43 +1300

    create update del

--- src/useAPI.js
+++ src/useAPI.js
@@ -5,22 +5,60 @@ export function useAPI({url, fetchOptions, handleError}) {
    
     const [data, setData] = useState({
         results: []
-    });
-    
+    })
+
+    const [filter, setFilter] = useState({
+        page: 1
+    })
+
+    const [loading, setLoading] = useState(false)
+
     const read = async params => {
-        const res = await request(url, { body: params, ...fetchOptions }, handleError);
+        setLoading(true);
+        const newFilter = {...filter, ...params};
+        newFilter.page = params && params.page ? params.page : 1;
+        const res = await request(url, {...fetchOptions, body: newFilter}, handleError);
+        setLoading(false);
         if(!res.error) {
             setData(res);
+            setFilter(newFilter);
         }
     }
 
     const create = async params => {
-        
+        setLoading(true);
+        const res = await request(url, {...fetchOptions, body: params, method: 'POST'}, handleError);
+        setLoading(false);
+        if(!res.error) {
+            read();
+        }
     }
-    
+
+    const update = async(key, params) => {
+        setLoading(true);
+        const res = await request(`${url}/${key}`, {...fetchOptions, body: params, method: 'PUT'}, handleError);
+        setLoading(false);
+        if(!res.error) {
+            read();
+        }
+    }
+
+    const del = async key => {
+        setLoading(true);
+        const res = await request(`${url}/${key}`, {...fetchOptions, method: 'DELETE'}, handleError);
+        setLoading(false);
+        if(!res.error) {
+            read();
+        }
+    }
 
     return {
+        create: useCallback(create, []),
         data,
-        read: useCallback(read, [])
-    };
+        del: useCallback(del, []),
+        filter,
+        loading,
+        read: useCallback(read, []),
+        update: useCallback(update, [])
+    }
 }
